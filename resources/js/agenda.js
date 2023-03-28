c55316d1467ad23c687bf8dce2a01208301af32f
CasperDevs <casperdevs@outlook.es>
2023-03-28 04:02:06 -0600

    first commit

---
+++ resources/js/agenda.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('agenda');
+    
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
